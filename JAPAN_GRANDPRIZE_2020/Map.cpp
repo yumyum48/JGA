d79f36befeaae2b47b9ef17ecb3049eb7920a60e
yumayuma48 <puxipu939@gmail.com>
2020-04-06 17:27:10 +0900

    マップ.cppと.h追加

---
+++ JAPAN_GRANDPRIZE_2020/Map.cpp
@@ -0,0 +1,14 @@
+#include "DxLib.h"
+
+// マップの表示
+void MapDisp() {
+
+
+	// マップの描画
+	//DrawGraph(, 0, g_pic.map[0], TRUE);
+	//DrawGraph(, 0, g_pic.map[1], TRUE);
+}
+// マップの動き
+void MapMove() {
+
+}
