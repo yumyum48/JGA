0a21168efe08df79961028e6a331e38a65ff77a1
yumayuma48 <puxipu939@gmail.com>
2020-04-07 13:31:41 +0900

    ロードピック修正

--- JAPAN_GRANDPRIZE_2020/LoadPic.cpp
+++ JAPAN_GRANDPRIZE_2020/LoadPic.cpp
@@ -6,8 +6,10 @@ int LoadPicture() {
 	if ((g_pic.Block = LoadGraph("image/block.bmp")) == -1) return -1;
 	if ((g_pic.QBlock = LoadGraph("image/Qblock.jpg")) == -1) return -1;
 	if ((g_pic.Kirby = LoadGraph("image/KirbyN.png")) == -1) return -1;
-	if (LoadDivGraph("image/map.png", 2, 2, 1, 1024, 768, g_pic.map, 0) == -1)
-		return -1;
+	//if (LoadDivGraph("image/mori_4.png", 4, 4, 1, 1088, 768, g_pic.map, 0) == -1)
+		//return -1;
+	
+	if ((g_pic.Block = LoadGraph("image/block.bmp")) == -1) return -1;
 	/*
 	if (LoadDivGraph("images/Cnt2.png", 10, 10, 1, 57, 80, g_NumImage, 0) == -1)
 		return -1;*/
