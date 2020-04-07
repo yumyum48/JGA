d042a9d0cff56fd0f0744e00c2b66d9724679968
yumayuma48 <puxipu939@gmail.com>
2020-04-07 18:16:09 +0900

    map関係の修正、mapinfo型のストラクトの修正メンバをfloatからintへ

--- JAPAN_GRANDPRIZE_2020/Map.cpp
+++ JAPAN_GRANDPRIZE_2020/Map.cpp
@@ -1,6 +1,11 @@
 #include "DxLib.h"
 #include "Struct.h"
 #include "Picture.h"
+#include "control.h"
+#include "Map.h"
+
+#define MAP_MAX		 (3)	// マップの最大数
+#define SCREEN_WIDTH (1024)	// 画面の横幅
 
 mapInfo g_map[3];
 // マップの表示
@@ -8,18 +13,37 @@ void MapDisp() {
 
 
 	// マップの描画
-	DrawGraph(g_map[0].x, g_map[0].y, g_pic.map[0], TRUE);
-	DrawGraph(g_map[1].x, g_map[1].y, g_pic.map[1], TRUE);
-	DrawGraph(g_map[2].x, g_map[2].y, g_pic.map[2], TRUE);
+	for (int i = 0; i < MAP_MAX; i++) {
+		DrawGraph(g_map[i].x, g_map[i].y, g_pic.map[i], TRUE);
+		
+		// マップが画面外に入ったら次のところにセットされる
+		if (g_map[i].x + SCREEN_WIDTH < 0) {
+			g_map[i].x = SCREEN_WIDTH + SCREEN_WIDTH;
+		}
+	}
+
+	
 }
 // マップの動き
 void MapMove() {
 
-	//if()
+	// スクロール処理
+	if (g_keyInfo.nowKey & PAD_INPUT_RIGHT) {
+		for(int i=0; i<MAP_MAX; i++)
+			Scroll(&g_map[i].x);
+	}
+
+
 }
 
 void MapInit() {
 	g_map[0].MapInit1();
 	g_map[1].MapInit2();
 	g_map[2].MapInit3();
+}
+
+void Scroll(float *mapX) {
+	int moveX = 8;
+	
+	*mapX -= moveX;
 }
