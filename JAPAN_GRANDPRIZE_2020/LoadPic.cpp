266c2ec819e37dbeb384fe13fcee00a550dfe941
ko-nayuki <nakamurayuuki0629@icloud.com>
2020-04-10 13:59:22 +0900

    画像アニメーション

--- JAPAN_GRANDPRIZE_2020/LoadPic.cpp
+++ JAPAN_GRANDPRIZE_2020/LoadPic.cpp
@@ -8,6 +8,13 @@ int LoadPicture() {
 	if ((g_pic.player[0] = LoadGraph("image/1.png")) == -1) return -1;
 	if ((g_pic.player[1] = LoadGraph("image/2.png")) == -1) return -1;
 	if ((g_pic.player[2] = LoadGraph("image/3.png")) == -1) return -1;
+	if ((g_pic.player[3] = LoadGraph("image/4.png")) == -1) return -1;
+	if ((g_pic.player[4] = LoadGraph("image/3.png")) == -1) return -1;
+	if ((g_pic.player[5] = LoadGraph("image/2.png")) == -1) return -1;
+	if ((g_pic.player[6] = LoadGraph("image/1.png")) == -1) return -1;
+	if ((g_pic.player[7] = LoadGraph("image/2.png")) == -1) return -1;
+	if ((g_pic.player[8] = LoadGraph("image/3.png")) == -1) return -1;
+	if ((g_pic.player[9] = LoadGraph("image/5.png")) == -1) return -1;
 	if ((g_pic.enemy = LoadGraph("image/enemy.png")) == -1) return -1;
 	if (LoadDivGraph("image/mori8sute.png", 24, 3, 8, 1024, 768, g_pic.map, 0) == -1)
 		return -1;
