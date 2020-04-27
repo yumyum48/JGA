fb956980c0beba169cd22d56c7d21fab8feed2bd
yumayuma48 <puxipu939@gmail.com>
2020-04-27 23:02:16 +0900

    トラップ更新

--- JAPAN_GRANDPRIZE_2020/Trap.cpp
+++ JAPAN_GRANDPRIZE_2020/Trap.cpp
@@ -1,24 +1,50 @@
 #include "DxLib.h" 
 #include "Struct.h"
 #include "control.h"
-struct trapInfo {
-	
-	int x, y;
-	void Init() {
-		x = 1300;
-		y = 610;
-	}
-};
-trapInfo g_trap;
+#include "Select.h"
+#include "Trap.h"
+
+
+													
+
+/*********************************************
+
+* トラップの表示
+
+*/////////////////////////////////////////////
 void TrapDisp() {
-	DrawBox(g_mouseInfo.mouseX, g_mouseInfo.mouseY, g_mouseInfo.mouseX + 10, g_mouseInfo.mouseY + 10, 0xFF0000, TRUE);
-
+	
+	
+	if (GetRand(5) == 1) g_trap.dispFlg = TRUE;											// ランダムでトラップを出現させるフラグをオンにする
+	
+	if (g_trap.dispFlg == TRUE) {
+		DrawFormatString(500, 0, 0xFF00FF, "トラップ出てますよ！");
+		DrawBox(g_trap.x, g_trap.y, g_trap.x + 100, g_trap.y + 100, 0xFFFFFF, TRUE);	// トラップを表示
+	}
 }
 
+
+/*********************************************
+
+* トラップの動き
+
+*/////////////////////////////////////////////
 void TrapMove() {
+	if (g_trap.dispFlg) {
+		g_trap.x -= g_speedLevel;														// トラップのX座標をスクロール速度と同じ値で加算させる
 
-}
+		if (g_trap.x + 100 < 0) {
+			g_trap.Init();																// トラップが画面外に入った場合初期化
+			g_trap.dispFlg = FALSE;
+		}
+	}
+}	
 
+/*********************************************
+
+* トラップの初期化
+
+*/////////////////////////////////////////////
 void TrapInit() {
 	g_trap.Init();
 }
