679f016acfaa051acb24b814378f7ca28e9f66e8
yumayuma48 <puxipu939@gmail.com>
2020-04-27 20:01:22 +0900

    マクロ.hを追加

---
+++ JAPAN_GRANDPRIZE_2020/Macro.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define ENEMY_MAX 5
+#define GROUND 450
+#define PLAYER_REDUCTION (0.8)		  // ƒvƒŒƒCƒ„[‚Ìk¬—¦
+#define WINDOW_WIDTH    (1280)        // ‰æ–Ê‚Ì‰¡•
+#define WINDOW_HEIGHT   (768)         // ‰æ–Ê‚Ìc•
