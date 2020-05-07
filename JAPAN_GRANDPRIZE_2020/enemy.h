efb43546cadea132e449d25f86e6206b64dd6800
ko-nayuki <nakamurayuuki0629@icloud.com>
2020-05-07 20:18:31 +0900

    マップによって雑魚敵倒した数のやつ

--- JAPAN_GRANDPRIZE_2020/enemy.h
+++ JAPAN_GRANDPRIZE_2020/enemy.h
@@ -6,7 +6,9 @@
 #define ENEMY_HEIGHT	 (545 * 0.2)// エネミーの縦幅
 extern enemyType g_enemy[ENEMY_MAX];	// 敵の情報
 
+extern const int ENEMY_BEAT_MAX[MAP_MAX];	//各ステージの雑魚敵を倒す数です
 
+extern int g_enemybeat;	// エネミーを倒した数をカウントするグローバル変数
 
 //extern int g_killCount;
 void MonsterDisp();		// 弱い敵、ボスの表示
