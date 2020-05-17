975f53981950a673af2884c3dc7d38073d195afc
yumayuma48 <puxipu939@gmail.com>
2020-05-17 23:53:33 +0900

    ロード追加！

--- JAPAN_GRANDPRIZE_2020/GameOver.cpp
+++ JAPAN_GRANDPRIZE_2020/GameOver.cpp
@@ -9,6 +9,7 @@
 #include "Init.h"
 #include "Rain.h"
 
+picInfo g_gameOverText;	// [“¢”°Ž¸”s]‰æ‘œ‚ÌÀ•W
 void GameOver() {
 	GameOverMove();
 	GameOverDisp();
@@ -22,23 +23,43 @@ void GameOverMove() {
 
 void GameOverDisp() {
 
-	static int motion = 0;
-
+	static int motion = 255;
+	SetDrawBlendMode(DX_BLENDMODE_ALPHA, motion);
 	MapDisp();
 	PlayerDisp();
 	RainDisp();
-	SetDrawBlendMode(DX_BLENDMODE_ALPHA, ++motion);
-	DrawBox(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 0x000000, TRUE);
 	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
-	if (motion > 255) {
-		DrawBox(WINDOW_WIDTH - (motion*4)+1000,200, WINDOW_WIDTH - (motion*4)+1500,400,0xFFFFFF,TRUE);
-	}
-	if (motion > 480) {
-		motion = 480;
-		if (g_keyInfo.keyFlg & PAD_INPUT_A) {
+
+	DrawRotaGraph2(g_gameOverText.x, g_gameOverText.y, 0, 0, 0.6, 0.0, g_pic.gameOverText, TRUE);
+	//if (motion > 255) {
+	//	DrawBox(WINDOW_WIDTH - (motion*4)+1000,200, WINDOW_WIDTH - (motion*4)+1500,400,0xFFFFFF,TRUE);
+	//}
+	motion -= 10;
+	if (motion < 100) {
+		motion = 100;
+		if( (GameOverTextMove() == 1)
+		&&  (g_keyInfo.keyFlg & PAD_INPUT_A) ){
 			GameInit();
 			g_gameScene = GAME_SELECT;
 			motion = 0;
 		}
 	}
+	//}
+}
+int GameOverTextMove() {
+	if (g_gameOverText.y < 298) {
+		g_gameOverText.y += 25;
+	}
+	else {
+		g_gameOverText.y = 298;
+		return 1;
+	}
+
+	return 0;
+}
+
+void GameOverInit() {
+	g_gameOverText.x = 279;
+	g_gameOverText.y = -237;
+
 }
--- JAPAN_GRANDPRIZE_2020/GameOver.h
+++ JAPAN_GRANDPRIZE_2020/GameOver.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "DxLib.h"
 
-void GameOver();
-void GameOverDisp();
-void GameOverMove();
+void GameOver();		// ゲームオーバー画面の管理
+void GameOverDisp();	// ゲームオーバー画面の表示
+void GameOverMove();	// ゲームオーバー画面の動	
+int GameOverTextMove(); // ゲームオーバー画面の[討伐失敗]の画像の動き
+void GameOverInit();	// ゲームオーバー画面の初期化
