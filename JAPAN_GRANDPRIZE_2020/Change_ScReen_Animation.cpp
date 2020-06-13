363db6b5368643ea032e20993c99807f74f567d0
yumayuma48 <puxipu939@gmail.com>
2020-06-13 17:54:48 +0900

    セレクトからゲームプレイへのアニメーション

--- JAPAN_GRANDPRIZE_2020/Change_ScReen_Animation.cpp
+++ JAPAN_GRANDPRIZE_2020/Change_ScReen_Animation.cpp
@@ -6,47 +6,73 @@
 #include "Map.h"
 #include "Player.h"
 #include "Rain.h"
+#include "Select.h"
+
+enum {
+	HUSUMA_LEFT,
+	HUSUMA_RIGHT,
+	HUSUMA_LEFT_CLOSE,
+	HUSUMA_RIGHT_CLOSE,
+	HUSUMA_OPEN,
+};
+
 animationInfo g_disp[2];
+int g_openFlg = HUSUMA_RIGHT_CLOSE;
+
 void ChangeScreen_Animation() {
 	
 	MapDisp();			// マップの表示
 	RainDisp();			// 雨の表示
 	RainMove();			// 雨の動き
 	PlayerDisp();		// プレイヤーの表示
+	
+	if (g_openFlg != HUSUMA_OPEN) {
+		SelectDisp();
+	}
 	scene_animaInput anima = Get_NowDisp(0, 0);
 	if (anima.animeType == 1) {
 		DrawGraph(g_disp[0].x, g_disp[0].y, anima.nowDisp[0], TRUE);
 	}
 	else if (anima.animeType == 2) {
 
-		DrawRotaGraph2(g_disp[0].x, g_disp[0].y, 0, 0, 2.0, 0.0, anima.nowDisp[0], TRUE);
-		DrawRotaGraph2(g_disp[1].x, g_disp[1].y, 0, 0, 2.0, 0.0, anima.nowDisp[1], TRUE);
+		DrawRotaGraph2(g_disp[0].x, g_disp[0].y, 0, 0, 8.0, 0.0, anima.nowDisp[0], TRUE);
+		DrawRotaGraph2(g_disp[1].x, g_disp[1].y, 0, 0, 8.0, 0.0, anima.nowDisp[1], TRUE);
 		Scene_Animation_Shoji(anima.nextScene);
-
 	}
-
-
-
 }
-void Scene_Animation_Shoji(int nextScene) {
-	static bool openFlg = FALSE;
-	if (openFlg == FALSE) {		// 閉めるアニメーション
-		if (g_disp[1].x < WINDOW_WIDTH) {
 
+void Scene_Animation_Shoji(int nextScene) {
+	int move = 15;
+	if (g_openFlg == HUSUMA_RIGHT_CLOSE) {		// 閉めるアニメーション
+		if (g_disp[HUSUMA_RIGHT].x >=  (WINDOW_WIDTH / 2)) {
+			g_disp[HUSUMA_RIGHT].x -= move;
+		}
+		else {
+			g_openFlg = HUSUMA_LEFT_CLOSE;
 		}
 	}
-	if ((g_disp[0].x + (WINDOW_WIDTH / 2)) + WINDOW_WIDTH > 0) {		// 左の障子
-		g_disp[0].x -= 10;
+	if (g_openFlg == HUSUMA_LEFT_CLOSE) {		// 閉めるアニメーション
+		if (g_disp[HUSUMA_LEFT].x + (WINDOW_WIDTH / 2) <= (WINDOW_WIDTH / 2)) {
+			g_disp[HUSUMA_LEFT].x += move;
+		}
+		else {
+			g_openFlg = HUSUMA_OPEN;
+		}
 	}
+	if (g_openFlg == HUSUMA_OPEN) {		// 閉めるアニメーション
+		if ((g_disp[HUSUMA_LEFT].x + (WINDOW_WIDTH / 2)) + WINDOW_WIDTH > 0) {		// 左の障子
+			g_disp[HUSUMA_LEFT].x -= move;
+		}
 
-	if (g_disp[1].x< WINDOW_WIDTH) {									// 右の障子
-		g_disp[1].x += 10;
+		if (g_disp[HUSUMA_RIGHT].x < WINDOW_WIDTH) {									// 右の障子
+			g_disp[HUSUMA_RIGHT].x += move;
+		}
+		else {
+			ChangeScreen_Animation_Init();									// アニメーションが終了していたら初期化
+			g_openFlg = HUSUMA_RIGHT_CLOSE;									// オープンフラグを初期化
+			g_gameScene = nextScene;										// シーン切り替え
+		}
 	}
-	else {
-		ChangeScreen_Animation_Init();									// アニメーションが終了していたら初期化
-		g_gameScene = nextScene;
-	}
-
 }
 scene_animaInput Get_NowDisp(int nextScene, int animeType) {	// シーンを変える時のアニメーションの情報を格納して返り値として返す関数 引数 (int 次のシーン, int 使用するアニメーションの番号)
 	
@@ -88,6 +114,8 @@ scene_animaInput Get_NowDisp(int nextScene, int animeType) {	// シーンを変える時
 
 void ChangeScreen_Animation_Init() {
 	for (int i = 0; i < 2; i++) {
-		g_disp[i].Init(i);
+		g_disp[i].Init(i);			// アニメーション用の画面を初期化する
 	}
+
+	g_openFlg = HUSUMA_RIGHT_CLOSE;	// オープンフラグを初期化
 }
