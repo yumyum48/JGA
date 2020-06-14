0d907fc285d796bff56b1581f16b3385dcb696e0
yoshiyuki-2 <yoshiyuki2124357@gmail.com>
2020-06-14 21:13:22 +0900

    音量調整

--- JAPAN_GRANDPRIZE_2020/Sounds.cpp
+++ JAPAN_GRANDPRIZE_2020/Sounds.cpp
@@ -51,6 +51,6 @@ int LoadSounds(void) {				//ƒTƒEƒ“ƒh“Ç‚İ‚İ
 void SoundsInit() {
 
 	for (int i = 0; i < BGM_MAX; i++) {
-		ChangeVolumeSoundMem(255 * 50 / 100, g_sound[i]);
+		ChangeVolumeSoundMem(255 * 25 / 100, g_sound[i]);
 	}
 }
